0fb331f279fec45b5056e9eaa959fdf1fd7f281a
BlueSky428 <bluesky.engineer428@gmail.com>
2020-06-05 08:57:07 -0400

    added font awesome svg import

--- pages/MyRecipes/Recipe/[recipeId].js
+++ pages/MyRecipes/Recipe/[recipeId].js
@@ -6,6 +6,7 @@ import XCircle from 'heroicons/solid/x-circle.svg';
 import ChevronLeft from 'heroicons/solid/chevron-left.svg';
 import Calendar from 'heroicons/outline/calendar.svg';
 import ClipboardList from 'heroicons/outline/clipboard-list.svg';
+import Save from '@fortawesome/fontawesome-free/svgs/regular/save.svg'
 
 function Recipe(recipe) {
   const [pageState, setPageState] = useState("view");
@@ -191,19 +192,8 @@ function RecipeActions({pageState, updatePageState}){
         onClick={saveRecipe()}
         type="button"
         className="inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-l-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          className="h-5 w-5"
-          fill="currentColor"
-        >
-          <path
-            fill="currentColor"
-            d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM272 80v80H144V80h128zm122 352H54a6 6 0 0 1-6-6V86a6 6 0 0 1 6-6h42v104c0 13.255 10.745 24 24 24h176c13.255 0 24-10.745 24-24V83.882l78.243 78.243a6 6 0 0 1 1.757 4.243V426a6 6 0 0 1-6 6zM224 232c-48.523 0-88 39.477-88 88s39.477 88 88 88 88-39.477 88-88-39.477-88-88-88zm0 128c-22.056 0-40-17.944-40-40s17.944-40 40-40 40 17.944 40 40-17.944 40-40 40z"
-            class
-          />
-        </svg>
+      >        
+        <Save className ="h-5 w-5 fill-current" />
         {/* <p v-if="this.saving" className="hidden sm:block sm:pl-2 md:hidden lg:block">Saving...</p>
         <p v-else className="hidden sm:block sm:pl-2 md:hidden lg:block">Save</p> */}
       </button>
